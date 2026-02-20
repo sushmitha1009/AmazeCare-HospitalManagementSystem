54b94724fa028e004fad7202cc815fbcebdcabfc
SUSHMITHA S <122036827+sushmitha1009@users.noreply.github.com>
2026-02-20 15:30:44 +0530

    Fix import path for BASE_URL in DoctorDashboard

--- src/pages/DoctorDashboard.js
+++ src/pages/DoctorDashboard.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './DoctorDashboard.css';
-import BASE_URL from "./config";
+import BASE_URL from "../config";
 
 export default function DoctorDashboard() {
   const [appointments, setAppointments] = useState([]);
