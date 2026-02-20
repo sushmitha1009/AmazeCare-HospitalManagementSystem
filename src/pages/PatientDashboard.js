b70d23ddcf377a2d0c7bb87d3ce4258191c86a0e
SUSHMITHA S <122036827+sushmitha1009@users.noreply.github.com>
2026-02-20 15:52:33 +0530

    Update import path for BASE_URL in PatientDashboard

--- src/pages/PatientDashboard.js
+++ src/pages/PatientDashboard.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './PatientDashboard.css';
-import BASE_URL from "../../config";
+import BASE_URL from "../config";
 
 export default function PatientDashboard() {
   const [activeTab, setActiveTab] = useState('profile');
