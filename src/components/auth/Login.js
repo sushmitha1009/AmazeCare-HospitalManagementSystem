bd5ffa1874e2cec0094df963aeedc9cbb013aa97
SUSHMITHA S <122036827+sushmitha1009@users.noreply.github.com>
2026-02-20 15:40:09 +0530

    Update Login.js

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../Login.css";
+import "./Login.css";
 import BASE_URL from "../config";
 
 export default function Login() {
